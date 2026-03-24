6c84b7192a1e3054bba708fd2f820c10ce57c694
whtn11 <elromevar7115@gmail.com>
2026-03-23 17:17:49 -0700

    Feat: agregar tarea 2 a win y linux

--- Script/Windows/tarea1.ps1
+++ Script/Windows/tarea1.ps1
@@ -1,9 +1,9 @@
-#!/bin/bash
-# check_status.sh - Tarea 1
 
-echo "============================="
-echo " NOMBRE DEL EQUIPO: $(hostname)"
-echo " IP ACTUAL: $(hostname -I | awk '{print $1}')"
-echo " ESPACIO EN DISCO:"
-df -h /
-echo "============================="
+# check_status.ps1 - Tarea 1
+
+Write-Host "============================="
+Write-Host " NOMBRE DEL EQUIPO: $($env:COMPUTERNAME)"
+Write-Host " IP ACTUAL: $((Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.IPAddress -ne '127.0.0.1'} | Select-Object -First 1).IPAddress)"
+Write-Host " ESPACIO EN DISCO:"
+Get-PSDrive C | Select-Object @{N='Total(GB)';E={[math]::Round(($_.Used+$_.Free)/1GB,2)}}, @{N='Usado(GB)';E={[math]::Round($_.Used/1GB,2)}}, @{N='Libre(GB)';E={[math]::Round($_.Free/1GB,2)}}
+Write-Host "============================="
